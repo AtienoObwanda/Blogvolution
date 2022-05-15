340d3f3a632db6742aa7d49ece0b1be097f741e9
atienoobwanda <atienoobwanda@gmail.com>
2022-05-15 17:26:59 +0300

    adding users table

--- app/models.py
+++ app/models.py
@@ -3,3 +3,16 @@ from flask_login import UserMixin
 from datetime import datetime
 
 from . import db, login_manager
+
+@login_manager.user_loader
+def login_user(user_id):
+    return User.query.get(int(user_id))
+
+
+# Users
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image = db.Column(db.String(120), nullable=False)
+    password = db.Column(db.String(60), nullable=False)
