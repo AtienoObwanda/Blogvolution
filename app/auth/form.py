4e07749ee6dd20b775f02fe1bd66553a6e8beb28
atienoobwanda <atienoobwanda@gmail.com>
2022-05-15 16:32:33 +0300

    adding the forms for registration, and login

--- app/auth/form.py
+++ app/auth/form.py
@@ -7,5 +7,78 @@ from flask_login import current_user
 
 class RegistrationForm(FlaskForm):
     '''
-    Define class
+    Defines class for registration
     '''
+    username = StringField('Username', 
+                    validators=[DataRequired(),Length(min=4, max=15)])
+    email = StringField('Email',validators=[DataRequired(),Email()])
+    password = PasswordField('Password',
+                    validators=[DataRequired(),Length(min=6,max=12),
+                    EqualTo('confirm_password',message = 'Passwords must match')])
+    confirm_password = PasswordField('Confirm Password',
+                    validators=[DataRequired(),EqualTo('password')])
+
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user= User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Username is already in use!')
+
+
+    def validate_email(self, email):
+        user= User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Email is already in use!')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',validators=[DataRequired(),Email()])
+    password = PasswordField('Password',
+                    validators=[DataRequired(),Length(min=6,max=12)])
+
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', 
+                    validators=[DataRequired(),Length(min=4, max=15)])
+    email = StringField('Email',validators=[DataRequired(),Email()])
+    
+    picture= FileField('Update profile picture',validators=[FileAllowed(['jpg','png','jpeg'])])
+    
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user= User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('Username is already in use!')
+
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user= User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('Email is already in use!')
+
+
+class RequestResetForm(FlaskForm):
+    email = StringField('Email',validators=[DataRequired(),Email()])
+
+    submit = SubmitField('Reset Password')
+
+    def validate_email(self, email):
+        user= User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError('There is no account with that email, kindly SIGNUP!')
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password',
+                    validators=[DataRequired(),Length(min=6,max=12),
+                    EqualTo('confirm_password',message = 'Passwords must match')])
+    confirm_password = PasswordField('Confirm Password',
+                    validators=[DataRequired(),EqualTo('password')])
+
+    submit = SubmitField('Confirm Reset Password')
