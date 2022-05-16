72ae39615507f85751fa728fae41ba256521ac03
atienoobwanda <atienoobwanda@gmail.com>
2022-05-16 17:36:55 +0300

    updating config file

--- config.py
+++ config.py
@@ -4,35 +4,55 @@ class Config:
     '''
     Parent config class
     '''
-
     SECRET_KEY =os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
+    SQLALCHEMY_TRACK_MODIFICATIONS=True
+    # Mail confugurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS=True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    SUBJECT_PREFIX = 'PITCH SPLASH!'
+    SENDER_EMAIL = 'splashpitch@gmail.com'
 
-    # For migrations
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://atieno:mishi@localhost/blogs'
-    # for prod
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') #.replace("://", "ql://", 1)
-    SQLALCHEMY_TRACK_MODIFICATIONS= False
+
+    @staticmethod
+    def init_app(app):
+        pass
 
 class ProdConfig(Config):
     '''
-    ProdConfig --> child class
+    Production  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
     '''
-    pass
-
-class DevConfig(Config):
-    '''
-    DevConfig --> child class
-    '''
-
-    # For migrations
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://atieno:mishi@localhost/blogs' 
     
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') #.replace("://", "ql://", 1)
+
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
 
 
 DEBUG = True
 
+
+class TestConfig(Config):
+    '''
+    Test
+    '''
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
+
+
 config_options = {
 'development':DevConfig,
-'production':ProdConfig
+'production':ProdConfig,
+'test':TestConfig,
 }
