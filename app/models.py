19b830aa908b87842895930131a1759225e226e7
atienoobwanda <atienoobwanda@gmail.com>
2022-05-16 17:45:18 +0300

    Deploying to Heroku

--- app/models.py
+++ app/models.py
@@ -64,13 +64,13 @@ class Quote:
         self.author = author
         self.quote = quote
 
-class Subscription(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    email = db.Column(db.String(255), unique = True, index = True)
+# class Subscription(db.Model):
+#     id = db.Column(db.Integer, primary_key = True)
+#     email = db.Column(db.String(255), unique = True, index = True)
 
-    def save_subscriber(self):
-        db.session.add(self)
-        db.session.commit()
+#     def save_subscriber(self):
+#         db.session.add(self)
+#         db.session.commit()
 
-    def __repr__(self):
-        return f'Subscriber {self.email}'
+#     def __repr__(self):
+#         return f'Subscriber {self.email}'
