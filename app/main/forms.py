4f8c3d5a1162887a7d581d318b5065543cee3fac
atienoobwanda <atienoobwanda@gmail.com>
2022-06-02 08:11:20 +0300

    C8

---
+++ app/main/forms.py
@@ -0,0 +1,19 @@
+from flask_wtf import FlaskForm
+from wtforms import TextAreaField,StringField,PasswordField, SubmitField, BooleanField
+from wtforms.validators  import DataRequired, ValidationError
+from flask_login import current_user
+
+
+
+
+class PostForm(FlaskForm):
+    
+    title = StringField('Post Title',validators=[DataRequired()])
+
+    content = TextAreaField('Post Content',validators=[DataRequired()])
+
+    submit = SubmitField('Publish Post')
+
+class CommentForm(FlaskForm):
+    comment = StringField('Comment')
+    submit = SubmitField('Comment')
